1ae2519971e9f6dddd6f761374780b75ef2e2081
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-09-15 19:53:54 -0300

    Requisito 10 completo - testes com as palavras CERTAS dessa vez

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,4 +2,8 @@ from src.counter import count_ocurrences
 
 
 def test_counter():
-    assert (count_ocurrences('src/jobs.csv', 'York')) == 598
+    # assert (count_ocurrences('src/jobs.csv', 'New York')) == 597
+    # assert (count_ocurrences('src/jobs.csv', 'Rego Park')) == 89
+    # assert (count_ocurrences('src/jobs.csv', 'Williston Park')) == 89
+    assert (count_ocurrences('src/jobs.csv', 'Python')) == 1639
+    assert (count_ocurrences('src/jobs.csv', 'Javascript')) == 122
